e39b7f28677e60863b8579d73cbf4a84a0111c65
hassanShakur <hass.shakurhassan@gmail.com>
2024-01-27 22:32:56 +0300

    initialize other pages components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
+import DashBoard from "@/components/DashBoard";
+
 export default function Home() {
   return (
-    <div className="grid gap-8 grid-cols-2 w-fit">
-      <div className='h-52 w-80 shadow-lg rounded-md bg-white p-4'>
-        <h3 className="font-semibold text-xl tracking-wide">Category</h3>
-      </div>
-      <div className='h-52 w-80 shadow-lg rounded-md bg-white p-4'>
-        <h3 className="font-semibold text-xl tracking-wide">Sales</h3>
-      </div>
-      <div className='h-52 w-80 shadow-lg rounded-md bg-white p-4'>
-        <h3 className="font-semibold text-xl tracking-wide">Product Type</h3>
-      </div>
-    </div>
+    <DashBoard />
   );
 }
---
+++ app/shelf/add.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddShelf = () => {
+  return <div>AddShelf</div>;
+};
+
+export default AddShelf;
