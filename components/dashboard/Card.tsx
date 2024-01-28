218a409fde0e82ad6d9f8b24563a92e3f5b88248
hassanShakur <hass.shakurhassan@gmail.com>
2024-01-28 10:40:40 +0300

    Refactor dashboard and header components

---
+++ components/dashboard/Card.tsx
@@ -0,0 +1,25 @@
+import { Card, CardHeader, CardMedia } from '@mui/material';
+
+interface MainCardProps {
+  item: {
+    id: number;
+    title: string;
+  };
+}
+
+const MainCard = ({ item }: MainCardProps) => {
+  return (
+    <Card className='min-w-full md:min-w-auto w-full max-w-40 h-64 shadow-lg rounded-md bg-white cursor-pointer hover:shadow-2xl transition duration-300 ease-in-out relative'>
+      <h3 className='font-semibold text-xl tracking-wide p-4'>
+        {item.title}
+      </h3>
+      <CardMedia
+        className='h-full w-full'
+        image={`https://source.unsplash.com/random/${item.id}`}
+        title='Random Image'
+      />
+    </Card>
+  );
+};
+
+export default MainCard;
--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,32 +1,28 @@
-import { Card } from '@mui/material';
+import MainCard from './Card';
 
 const cardItems = [
   {
+    id: 1,
     title: 'Category',
-    value: '5',
+    color: 'zinc', // 700,400
   },
   {
+    id: 2,
     title: 'Sales',
-    value: '5',
+    color: 'amber', // 600,100
   },
   {
+    id: 3,
     title: 'Product Type',
-    value: '5',
+    color: 'sky', // 600,100
   },
 ];
 
 const Dashboard = () => {
   return (
-    <div className='grid gap-8 grid-cols-1 sm:grid-cols-2 w-[90vw] px-10 sm:px-2 mx-auto md:mx-0 sm:w-full md:max-w-screen-sm'>
+    <div className='grid gap-8 grid-cols-1 sm:grid-cols-2 w-[85vw] px-2 sm:px-2 mx-auto md:mx-0 sm:w-full md:max-w-screen-sm'>
       {cardItems.map((item, index) => (
-        <Card
-          key={index}
-          className='min-w-full md:min-w-auto w-full max-w-40 h-52 shadow-lg rounded-md bg-white p-4 cursor-pointer hover:shadow-2xl transition duration-300 ease-in-out'
-        >
-          <h3 className='font-semibold text-xl tracking-wide'>
-            {item.title}
-          </h3>
-        </Card>
+        <MainCard key={index} item={item} />
       ))}
     </div>
   );
