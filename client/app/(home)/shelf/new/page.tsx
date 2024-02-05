137c0768c30afa045dd38fddfca9e43b450cbb17
hassanShakur <hass.shakurhassan@gmail.com>
2024-02-05 21:41:38 +0300

    Refactor AddShelf component and implement form state handling

--- client/app/(home)/shelf/new/page.tsx
+++ client/app/(home)/shelf/new/page.tsx
@@ -1,16 +1,31 @@
-import {
-  Button,
-  Grid,
-  InputAdornment,
-  InputLabel,
-  TextField,
-} from '@mui/material';
-import React from 'react';
+'use client';
+
+import { InputAdornment, InputLabel, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 const AddShelf = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    id: '',
+    category: '',
+    prodType: '',
+    product: '',
+    prodId: '',
+    prodDesc: '',
+    price: '',
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
   return (
     <div className='w-[85vw] px-2 sm:px-2 mx-auto md:mx-0 sm:w-full md:max-w-screen-sm'>
-      <form className='grid gap-4 grid-cols-2 '>
+      <form
+        className='grid gap-4 grid-cols-2'
+        onSubmit={handleSubmit}
+      >
         <div className='col-span-2 flex gap-10 justify-start items-center'>
           <InputLabel
             htmlFor='name'
@@ -24,6 +39,12 @@ const AddShelf = () => {
             size='small'
             placeholder='Shelf 1 Name'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                name: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -40,6 +61,12 @@ const AddShelf = () => {
             size='small'
             placeholder='001'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                id: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -56,6 +83,12 @@ const AddShelf = () => {
             size='small'
             placeholder='Category 1'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                category: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -72,6 +105,12 @@ const AddShelf = () => {
             size='small'
             placeholder='cereals, snacks, etc.'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                prodType: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -88,6 +127,12 @@ const AddShelf = () => {
             size='small'
             placeholder='bread, milk, etc.'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                product: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -104,6 +149,12 @@ const AddShelf = () => {
             size='small'
             placeholder='001'
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                prodId: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -122,6 +173,12 @@ const AddShelf = () => {
             multiline
             rows={5}
             className='flex-1'
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                prodDesc: e.target.value,
+              }))
+            }
           />
         </div>
 
@@ -142,6 +199,12 @@ const AddShelf = () => {
                 <InputAdornment position='start'>$</InputAdornment>
               ),
             }}
+            onChange={(e) =>
+              setFormState((prevState) => ({
+                ...prevState,
+                price: e.target.value,
+              }))
+            }
           />
         </div>
 
