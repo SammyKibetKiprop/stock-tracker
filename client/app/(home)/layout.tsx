0d35166d9dfc837ca1fafb251f4c778db321471a
hassanShakur <hass.shakurhassan@gmail.com>
2024-03-01 21:00:03 +0300

    setup nextui

--- client/app/(home)/layout.tsx
+++ client/app/(home)/layout.tsx
@@ -1,12 +1,15 @@
-import type { Metadata } from 'next';
+'use client';
+// import type { Metadata } from 'next';
 
 import NavigationPanel from '@/components/navigation';
+import { NextUIProvider } from '@nextui-org/react';
 
+// export const metadata: Metadata = {
+//   title: 'Stock Tracker | Home',
+//   description: 'Stock tracker IOT project management interface',
+// };
 
-export const metadata: Metadata = {
-  title: 'Stock Tracker | Home',
-  description: 'Stock tracker IOT project management interface',
-};
+import { Providers } from '../providers';
 
 export default function HomeLayout({
   children,
@@ -15,11 +18,13 @@ export default function HomeLayout({
 }>) {
   return (
     <>
-      <NavigationPanel />
-      {/* px-2 sm:px-2 mx-auto md:mx-0 sm:w-full md:max-w-screen-sm */}
-      <main className='h-[90vh] min-h-96 overflow-scroll mt-[10vh] md:mt-0 md:fixed bottom-0 right-0 w-screen md:w-[75vw] lg:w-[80vw] p-8'>
-        {children}
-      </main>
+      <Providers>
+        <NavigationPanel />
+        {/* px-2 sm:px-2 mx-auto md:mx-0 sm:w-full md:max-w-screen-sm */}
+        <main className='h-[90vh] min-h-96 overflow-scroll mt-[10vh] md:mt-0 md:fixed bottom-0 right-0 w-screen md:w-[75vw] lg:w-[80vw] p-8'>
+          {children}
+        </main>
+      </Providers>
     </>
   );
 }
