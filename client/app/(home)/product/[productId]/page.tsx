e954fcd76cafb25e36a8ede7d972121449c1c2b6
hassanShakur <hass.shakurhassan@gmail.com>
2024-03-02 07:57:24 +0300

    implement product edit modal

--- client/app/(home)/product/[productId]/page.tsx
+++ client/app/(home)/product/[productId]/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { useDisclosure } from '@nextui-org/react';
 import { useParams } from 'next/navigation';
 
 import { Product } from '@/utils/interfaces';
 import { getProduct } from '@/utils/service';
 import { Button } from '@mui/material';
+import ProductEditModal from '@/components/product/EditModal';
 
 const ProductDetail = () => {
   const [product, setProduct] = useState({} as Product);
+  const {
+    isOpen: isModalOpen,
+    onOpen: onModalOpen,
+    onOpenChange: onModalOpenChange,
+  } = useDisclosure();
   const { productId } = useParams();
 
   useEffect(() => {
@@ -23,41 +30,54 @@ const ProductDetail = () => {
   if (!product) return <div>Loading...</div>;
 
   return (
-    <div className='flex flex-col gap-5 items-start justify-start w-full min-w-[15rem]'>
-      <h3 className='font-bold tracking-wide text-xl capitalize mb-10'>
-        Product Detail
-      </h3>
-
-      <div className='p-6 shadow-md grid gap-6 w-full max-w-[35rem]'>
-        <h3 className='font-semibold tracking-wide text-xl capitalize mb-4'>
-          {product.name}
+    <>
+      <div className='flex flex-col gap-5 items-start justify-start w-full min-w-[15rem]'>
+        <h3 className='font-bold tracking-wide text-xl capitalize mb-10'>
+          Product Detail
         </h3>
-        <p className='text-gray-500 text-[1.1rem] leading-6'>
-          {product.description}
-        </p>
-        <div className='flex justify-between items-center'>
-          <p className='text-[1.1rem] leading-6 font-semibold text-green-600'>
-            ${product.price}
-          </p>
-          <p
-            className='text-gray-500 text-[1.1rem] leading-6'
-            style={{ fontWeight: 500 }}
-          >
-            Shelf {product.shelfId}
-          </p>
-        </div>
 
-        {/* actions = edit, delete */}
-        <div className='flex gap-4 justify-end mt-6'>
-          <button className='text-white bg-slate-700 px-4 py-1 rounded-sm w-20 hover:bg-slate-600'>
-            Edit
-          </button>
-          <button className='text-white bg-red-500 px-4 py-1 rounded-sm w-20 hover:bg-red-400'>
-            Delete
-          </button>
+        <div className='p-6 shadow-md grid gap-6 w-full max-w-[35rem]'>
+          <h3 className='font-semibold tracking-wide text-xl capitalize mb-4'>
+            {product.name}
+          </h3>
+          <p className='text-gray-500 text-[1.1rem] leading-6'>
+            {product.description}
+          </p>
+          <div className='flex justify-between items-center'>
+            <p className='text-[1.1rem] leading-6 font-semibold text-green-600'>
+              ${product.price}
+            </p>
+            <p
+              className='text-gray-500 text-[1.1rem] leading-6'
+              style={{ fontWeight: 500 }}
+            >
+              Shelf {product.shelfId}
+            </p>
+          </div>
+
+          {/* actions = edit, delete */}
+          <div className='flex gap-4 justify-end mt-6'>
+            <button
+              className='text-white bg-slate-700 px-4 py-1 rounded-md text-sm w-20 hover:bg-slate-600'
+              onClick={onModalOpen}
+            >
+              Edit
+            </button>
+            <button className='text-white bg-red-500 px-4 py-1 rounded-md text-sm w-20 hover:bg-red-400'>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <ProductEditModal
+        product={product}
+        editMode={true}
+        isModalOpen={isModalOpen}
+        onModalOpen={onModalOpen}
+        onModalOpenChange={onModalOpenChange}
+      />
+    </>
   );
 };
 
---
+++ client/components/product/EditModal.tsx
@@ -0,0 +1,135 @@
+import { Product } from '@/utils/interfaces';
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+} from '@nextui-org/react';
+import { useState } from 'react';
+
+interface ProductEditModalProps {
+  product: Product;
+  editMode: boolean;
+  isModalOpen: boolean;
+  onModalOpen: () => void;
+  onModalOpenChange: (isOpen: boolean) => void;
+}
+
+const ProductEditModal = ({
+  product,
+  editMode,
+  isModalOpen,
+  onModalOpen,
+  onModalOpenChange,
+}: ProductEditModalProps) => {
+  const emptyProduct: Product = {
+    id: '',
+    name: '',
+    description: '',
+    price: 0,
+    shelfId: '',
+    image: '',
+  };
+
+  const [productData, setProductData] = useState<Product>(
+    editMode ? product : emptyProduct
+  );
+
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onOpenChange={onModalOpenChange}
+      placement='top-center'
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className='flex flex-col gap-1'>
+              Edit Product
+            </ModalHeader>
+            <ModalBody>
+              <Input
+                autoFocus
+                label='text'
+                placeholder='Enter product name'
+                variant='bordered'
+                value={productData.name}
+                onChange={(e) =>
+                  setProductData({
+                    ...productData,
+                    name: e.target.value,
+                  })
+                }
+              />
+              <Input
+                label='Description'
+                placeholder='Enter product description'
+                type='textarea'
+                variant='bordered'
+                value={productData.description}
+                onChange={(e) =>
+                  setProductData({
+                    ...productData,
+                    description: e.target.value,
+                  })
+                }
+              />
+              <Input
+                label='Price'
+                placeholder='Enter product price'
+                type='number'
+                variant='bordered'
+                value={`${productData.price}`}
+                onChange={(e) =>
+                  setProductData({
+                    ...productData,
+                    price: +e.target.value,
+                  })
+                }
+              />
+              <Input
+                label='Shelf ID'
+                placeholder='Enter shelf ID'
+                type='number'
+                variant='bordered'
+                value={productData.shelfId}
+                onChange={(e) =>
+                  setProductData({
+                    ...productData,
+                    shelfId: e.target.value,
+                  })
+                }
+              />
+              <Input
+                label='Image'
+                placeholder='Enter image URL'
+                type='url'
+                variant='bordered'
+                value={productData.image}
+                onChange={(e) =>
+                  setProductData({
+                    ...productData,
+                    image: e.target.value,
+                  })
+                }
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button color='danger' variant='flat' onPress={onClose}>
+                Cancel
+              </Button>
+              <Button color='primary' onPress={onClose}>
+                Save
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ProductEditModal;
